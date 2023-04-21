a85252bf0122e74fdd8e456aa3a6689c15848282
hexomethyl <86240431+hexomethyl@users.noreply.github.com>
2023-04-21 15:45:26 +1000

    Fix mixing of G and B in color constructor

--- src/color.cpp
+++ src/color.cpp
@@ -143,8 +143,8 @@ renderer::color_rgba::color_rgba(uint8_t r, uint8_t g, uint8_t b, uint8_t a) : r
 
 renderer::color_rgba::color_rgba(const glm::vec4& f) {
 	r = static_cast<uint8_t>(static_cast<uint32_t>(f.r * 255.0f));
-	g = static_cast<uint8_t>(static_cast<uint32_t>(f.b * 255.0f));
-	b = static_cast<uint8_t>(static_cast<uint32_t>(f.g * 255.0f));
+	g = static_cast<uint8_t>(static_cast<uint32_t>(f.g * 255.0f));
+	b = static_cast<uint8_t>(static_cast<uint32_t>(f.b * 255.0f));
 	a = static_cast<uint8_t>(static_cast<uint32_t>(f.a * 255.0f));
 }
 
